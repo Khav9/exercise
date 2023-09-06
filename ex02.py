1655d27848c4ab5c364325e277ad0a8920bdb45d
Khav9 <khav.saroeun@student.passerellesnumeriques.org>
2023-09-06 07:15:27 +0700

    ex02

---
+++ ex02.py
@@ -0,0 +1,10 @@
+array = [ ["B","A","N","A","N","A"],["H","I"]]
+newArray = []
+for i in range(len(array)):
+    word = ""
+    for j in range(len(array[i])):
+        word += array[i][j]
+    newArray.append(word)
+
+print(newArray)
+    
